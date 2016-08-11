91f941e7056f93255a99e69ebb126dfcdf3275ff
KoljaWindeler <Kolja.Windeler@gmail.com>
2016-08-11 22:08:20 +0200

    removed dhclient from script

--- overlay/load.sh
+++ overlay/load.sh
@@ -3,7 +3,6 @@ sudo rmdir /sys/kernel/config/device-tree/overlays/JKW_HUMUS/ >/dev/null 2>&1;
 sudo mkdir /sys/kernel/config/device-tree/overlays/JKW_HUMUS
 dmesg >/tmp/pre_JKW_HUMUS_load
 su -c 'cat '$DIR'/JKW_humus.dtbo > /sys/kernel/config/device-tree/overlays/JKW_HUMUS/dtbo'
-dhclient -v eth0&;
 sleep 3
 dmesg >/tmp/post_JKW_HUMUS_load
 diff /tmp/pre_JKW_HUMUS_load /tmp/post_JKW_HUMUS_load
