5a1dba668cfc52fc828116defef6097bb900a3e4
Kolja Windeler <Kolja.Windeler@gmail.com>
2016-08-17 19:07:16 +0000

    added install early script

---
+++ overlay/install/install_pure.sh
@@ -0,0 +1,1 @@
+cp ../JKW_humus.dtbo /lib/firmware/nextthingco/chip/dip-2a-3.dtbo
---
+++ overlay/install/install_vga.sh
@@ -0,0 +1,2 @@
+cp ../JKW_humus.dtbo /lib/firmware/nextthingco/chip/dip-2a-3.dtbo
+cp /lib/firmware/nextthingco/chip/early/dip-9d011a-2.dtbo /lib/firmware/nextthingco/chip/early/dip-2a-3.dtbo
