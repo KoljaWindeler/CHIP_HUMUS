5f018df78a7ceb4c6c11699f19be557c182efcb9
KoljaWindeler <Kolja.Windeler@gmail.com>
2016-08-29 20:40:02 +0200

    fixed install

--- overlay/install.sh
+++ overlay/install.sh
@@ -1,7 +1,7 @@
-cp JKW_humus.dtbo /lib/firmware/nextthingco/chip/dip-2a-3.dtbo
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+cp $DIR"/JKW_humus.dtbo /lib/firmware/nextthingco/chip/dip-2a-3.dtbo"
 echo "w1_ds2431" >> /etc/modules
 
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 #write out current crontab
 crontab -l > mycron
 #echo new cron into cron file
