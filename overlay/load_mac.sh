804396f903712e8a131ba99824216c624cd3fbe8
Kolja Windeler <Kolja.Windeler@gmail.com>
2016-11-22 08:56:41 +0100

    Update load_mac.sh

--- overlay/load_mac.sh
+++ overlay/load_mac.sh
@@ -4,7 +4,7 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 sudo rmdir /sys/kernel/config/device-tree/overlays/JKW_HUMUS/ >/dev/null 2>&1;
 sudo mkdir /sys/kernel/config/device-tree/overlays/JKW_HUMUS
-su -c 'cat '$DIR'/JKW_humus.dtbo > /sys/kernel/config/device-tree/overlays/JKW_HUMUS/dtbo'
+su -c 'cat '$DIR'/dip-2a-1.dtbo > /sys/kernel/config/device-tree/overlays/JKW_HUMUS/dtbo'
 
 sleep 3
 
