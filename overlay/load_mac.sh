3125c73c4b6485175e8acaeceda2dca91e62b81b
Kolja Windeler <Kolja.Windeler@gmail.com>
2016-11-22 16:21:38 +0100

    Update load_mac.sh

--- overlay/load_mac.sh
+++ overlay/load_mac.sh
@@ -2,10 +2,6 @@
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-sudo rmdir /sys/kernel/config/device-tree/overlays/JKW_HUMUS/ >/dev/null 2>&1;
-sudo mkdir /sys/kernel/config/device-tree/overlays/JKW_HUMUS
-su -c 'cat '$DIR'/dip-2a-1.dtbo > /sys/kernel/config/device-tree/overlays/JKW_HUMUS/dtbo'
-
 sleep 3
 
 # Define the MAC address to be assigned to eth0.  If this is the
